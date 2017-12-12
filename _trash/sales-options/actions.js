467adc8f229f65782d6ae439c79720e386f3e6fd
jpa <jpa@wksjpa>
2017-12-12 21:58:52 +0100

    _trash removed

--- _trash/sales-options/actions.js
+++
@@ -1,3 +0,0 @@
-export const setProductOptions = ({ commit }, { options }) => {
-  commit('SET_PRODUCT_OPTIONS', { options })
-}
--- _trash/sales-products/mutations.js
+++
@@ -1,9 +0,0 @@
-export const SET_FAMILY = (state, { data }) => {
-  state.family = data
-}
-
-export const SET_PRODUCT_OPTIONS = (state, { options }) => {
-  state.productOptions = options.map((option) => {
-    return Object.assign({}, option)
-  })
-}
